338f9f42ae0534b1cd460a426c0fa15cfdbc46d8
Drakesanch36 <101512801+Drakesanch36@users.noreply.github.com>
2022-03-13 11:37:13 -0500

    top5

--- clt.py
+++ clt.py
@@ -7,8 +7,10 @@ desc = data.describe()
 cpp_data2 = data[['Company','Total','Lineitem name','Lineitem sku']].dropna()
 clt2 = cpp_data2.groupby(['Company','Lineitem sku'])['Total'].sum().to_frame('Total').reset_index()
 clt_order2 = clt2.sort_values(by =['Company','Total'], axis = 0, ascending = False, ignore_index = True)
+top5 = clt_order2.groupby('Company').head(5)
+#this is to only show the top 1 sku
 # clt_duplicate = clt_order2.drop_duplicates(subset=['Company'], keep='first')
             #displays all data
 pd.set_option("display.max_rows", None, "display.max_columns", None)
-print('Top SKU for each Company:')
-print(clt_order2)
+print('Top 5 SKU for each Company:')
+print(top5)
