f599b43acb76be34d9b74c65e032abd7b30e0a34
Drakesanch36 <101512801+Drakesanch36@users.noreply.github.com>
2022-03-26 13:09:17 -0500

    prophet

--- sf.py
+++ sf.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas import to_datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -10,6 +11,7 @@ from datetime import datetime, date, time, timedelta
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import pickle
 import fbprophet
+from fbprophet import Prophet
 # from feature_selector import FeatureSelector
 #this UAH Customer order data includes the hour
 data = pd.read_csv("UAH_customer_order_data2.csv")
@@ -24,12 +26,15 @@ data['dayofweek'] = data['Paid at'].dt.dayofweek
 data['weekofyear'] = data['Paid at'].dt.week
 data['Hour'] = data['Paid at'].dt.hour
 
+df = data[['Paid at', 'Lineitem quantity', 'Lineitem price']].dropna().sort_values(by="Paid at").reset_index()
+df['y'] = df['Lineitem quantity'] * df['Lineitem price']
+df.drop(['Lineitem quantity','Lineitem price'], axis=1)
 
-#this will give me the sales week by week
-sf = data[['weekofyear','Lineitem price','Lineitem quantity']].dropna().sort_values(by="weekofyear",ascending=True)
+
+sf = data[['weekofyear','Lineitem price','Lineitem quantity']].dropna().sort_values(by="weekofyear",ascending=True).reset_index()
 sf['Sales'] = sf['Lineitem price'] * sf['Lineitem quantity']
-
-print(sf)
+print(df)
+# print(sf)
 
 # Average weekly sales
 
@@ -44,8 +49,8 @@ print(f"Last 12 months average weekly sales: ${avg_weekly_sales_12month}")
 
 sf_week = sf.groupby(['weekofyear']).agg({'Sales':'sum'}).reset_index()
 # print(sf_week)
-plt.plot(sf_week.weekofyear, sf_week.Sales)
-plt.show()
+# plt.plot(sf_week.weekofyear, sf_week.Sales)
+# plt.show()
 
 #end of weekly data
 
@@ -65,5 +70,26 @@ print(f"Last 12 months average monthly sales: ${avg_monthly_sales_12month}")
 
 sf_month = sf1.groupby(['Month']).agg({'Sales':'sum'}).reset_index()
 # print(sf_week)
-plt.plot(sf_month.Month, sf_month.Sales)
-plt.show()
+# plt.plot(sf_month.Month, sf_month.Sales)
+# plt.show()
+dfcons = df[['Paid at', 'y']].sort_values(by='Paid at',ascending=True)
+#columns need to be ds and y for it to run properly
+df1 = dfcons.rename(columns={"Paid at": "ds"})
+df1['ds'] = to_datetime(df1['ds'])
+print(df1)
+#defines model
+model = Prophet()
+#fit the model
+model.fit(df1)
+
+# define the period for which we want a prediction
+future = list()
+for i in range(1, 13):
+	date = '2021-%02d' % i
+	future.append([date])
+future = DataFrame(future)
+future.columns = ['ds']
+future['ds']= to_datetime(future['ds'])
+
+# summarize the forecast
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head())
